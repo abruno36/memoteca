cefafd81182f9c599d6fa28a4a906949f909eaf0
abruno36 <abruno36@gmail.com>
2023-08-31 07:47:49 -0300

    Favoritar um pensamento

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,9 +1,7 @@
 import { Router } from '@angular/router';
 import { PensamentoService } from './../pensamento.service';
-import { Pensamento } from './../pensamento';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { minusculoValidator } from './minusculoValidator';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -28,7 +26,8 @@ export class CriarPensamentoComponent implements OnInit {
       autoria: ['', Validators.compose([
         Validators.required,
         Validators.minLength(3)])],
-      modelo: ['']
+       modelo: [''],
+       favorito: [false]
     })
   }
 
